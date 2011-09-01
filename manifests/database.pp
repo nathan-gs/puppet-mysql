5151a6eccf75e144fa14809844f8f50835f83431
Nathan Bijnens <nathan@nathan.gs>
2011-09-01 10:52:46 +0200

    mysql::database -> ' to "

--- manifests/database.pp
+++ manifests/database.pp
@@ -3,8 +3,8 @@ define mysql::database (
 ) {
 	
 	$sql = $ensure ? {
-	 'present'      => 'CREATE DATABASE ${name} IF NOT EXISTS',
-	 'absent'		=> 'DROP DATABASE ${name} IF EXISTS'
+	 'present'      => "CREATE DATABASE ${name} IF NOT EXISTS",
+	 'absent'		=> "DROP DATABASE ${name} IF EXISTS"
 	}
 	
 	mysql::sql { $sql:
