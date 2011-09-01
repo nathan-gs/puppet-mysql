4067a42ed72091266efbe698454902eacb5c6535
Nathan Bijnens <nathan@nathan.gs>
2011-09-01 09:52:42 +0200

    mysql::database -> create/drop db

---
+++ manifests/database.pp
@@ -0,0 +1,12 @@
+define mysql::database (
+	ensure		=> 'present',
+) {
+	
+	$sql = $ensure ? {
+	 'present'      => 'CREATE DATABASE ${name} IF NOT EXISTS',
+	 'absent'		=> 'DROP DATABASE ${name} IF EXISTS'
+	}
+	
+	mysql::sql { $sql }
+
+}
