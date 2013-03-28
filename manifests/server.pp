339cb9a10a8c35067986135fcb32cd3ada19ff06
Nathan Bijnens <nathan@nathan.gs>
2013-03-28 17:21:30 +0100

    typo during merge

--- manifests/server.pp
+++ manifests/server.pp
@@ -20,19 +20,13 @@ class mysql::server inherits mysql::base {
 		notify		=> Service["mysql"]
 	}
 
-	if ($operationsystem == 'debian') {
-		order		=> '700',
-		content		=> template("mysql/utf8.erb"),
-		notify		=> Service["mysql"]
-	}
-	
 	mysql::config { "utf8" :
 		order		=> '700',
 		content		=> template("mysql/utf8.erb"),
 		notify		=> Service["mysql"]
 	}
 
-	
+	if ($operationsystem == 'debian') {
 	    mysql::user { "root-localhost" :
             user		=> 'root',
             ensure		=> absent,
