13181f6dd45e4ae27891d4b27c725424c24385c5
Nathan Bijnens <nathan@nathan.gs>
2013-04-18 16:00:50 +0200

    innodb: use_plugin default on false.

--- manifests/config/innodb.pp
+++ manifests/config/innodb.pp
@@ -1,10 +1,10 @@
 define mysql::config::innodb (
-	$use_plugin 	= true,
+	$use_plugin 	= false,
 	$file_per_table	= true,
 	$order			= 100
 ) {
-	
-	mysql::config { "innodb" : 
+
+	mysql::config { "innodb" :
 		content		=> template('mysql/innodb.erb'),
 		order		=> $order,
 	}
